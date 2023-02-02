1fc3b295041c6ea0fac015aee6633b20fc24a506
mbialowas10 <mbialowas@rrc.ca>
2023-02-02 16:21:23 -0600

    Initial commit

---
+++ app/src/main/java/com/example/fragmentationproject/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fragmentationproject;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button_frag1;
+    Button button_frag2;
+    //TextView outer_result;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // get reference to controls
+    }
+}
